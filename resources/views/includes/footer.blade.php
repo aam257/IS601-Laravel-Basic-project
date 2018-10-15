1973bd72fa2c10b15f75ac77bca8ae3b8c80a74b
aniket mirajkar <aam257@njit.edu>
2018-10-15 14:16:10 -0400

    Fix: Footer link fixed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2018 Copyright:
-        <a href="/pages.home"> Laravel Basic Project</a>
+        <a class="nav-link" href="/"> Laravel Basic Project</a>
     </div>
     <!-- Copyright -->
 
