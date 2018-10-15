e95cef6a7106d854c922cad3e3e75264ce5a15fd
aniket mirajkar <aam257@njit.edu>
2018-10-15 13:56:33 -0400

    Feature: Default layout created.

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        @include('includes.head')
+    </head>
+
+    <body>
+        <div class="container">
+            <header>
+                @include('includes.header')
+            </header>
+
+            <div id="main" class="row">
+                @yield('content')
+            </div>
+
+
+            <footer class="row">
+                 @include('includes.footer')
+            </footer>
+
+        </div>
+    </body>
+
+
+
+
+</html>
